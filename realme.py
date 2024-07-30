aaa7c39743ed1f94d7fdde3f3867e6d22ceb25a0
rishav <rishav7822@gmail.com>
2024-07-30 08:48:58 +0530

    new files

--- realme.py
+++ realme.py
@@ -1,1 +1,13 @@
+class student:
+
+    def __init__(self, x, y):
+        self.name = x
+        self.age = y
+
+    def __str__(self):
+
+        return f"my name is {x} and age is {age}"
+
+s1 = student()
+
 
